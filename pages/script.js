e031486a70c02b583d494cd71f7cf9e734e00ba5
pstaal <peter.staal@gmail.com>
2022-02-14 08:42:02 +0100

    feature: display proper form values

--- pages/script.js
+++ pages/script.js
@@ -3,12 +3,22 @@ let editButton = document.querySelector('.profile__button-name-change');
 let closeButton = document.querySelector('.popup__close');
 
 function openModal(){
-    popup.classList.add('popup__open');
+    popup.classList.add('popup__opened');
 }
 
 function closeModal(){
-    popup.classList.remove('popup__open');
+    popup.classList.remove('popup__opened');
 }
 
 editButton.addEventListener('click', openModal);
 closeButton.addEventListener('click', closeModal);
+
+//set values of textinputs
+let formInputs = document.querySelectorAll('.popup__input');
+let nameInput = formInputs[0];
+let expertiseInput = formInputs[1];
+let nameValue = document.querySelector('.profile__name').textContent;
+let expertiseValue = document.querySelector('.profile__function').textContent;
+
+nameInput.value = nameValue;
+expertiseInput.value = expertiseValue;
