15e820adebb3056383efd48334b6f1faf0589dd0
pstaal <peter.staal@gmail.com>
2022-02-15 14:49:23 +0100

    hotfix: debugging all issues

---
+++ pages/scripts/script.js
@@ -0,0 +1,51 @@
+let popup = document.querySelector('.popup');
+let editButton = document.querySelector('.profile__button-name-change');
+let closeButton = document.querySelector('.popup__close');
+let nameInput = document.querySelector("[name='name']");
+let expertiseInput = document.querySelector("[name='function']");
+let profileName = document.querySelector('.profile__name');
+let profileFunction = document.querySelector('.profile__function');
+
+
+function openModal(){
+    //set values of textinputs
+    let nameValue = document.querySelector('.profile__name').textContent;
+    let expertiseValue = document.querySelector('.profile__function').textContent;
+    nameInput.value = nameValue;
+    expertiseInput.value = expertiseValue;
+    popup.classList.add('popup_opened');
+}
+
+function closeModal(){
+    popup.classList.remove('popup_opened');
+}
+
+editButton.addEventListener('click', openModal);
+closeButton.addEventListener('click', closeModal);
+
+
+
+
+
+//change the name and function in the DOM
+let formElement = document.querySelector('.popup__form'); 
+
+function handleProfileFormSubmit(evt) {
+    
+    evt.preventDefault();
+    
+    let newName = nameInput.value;
+    let newExpertise = expertiseInput.value;
+
+    profileName.textContent = newName;
+    profileFunction.textContent = newExpertise;
+
+    //close modal
+    closeModal();
+
+}
+
+// Connect the handler to the form:
+// it will watch the submit event
+formElement.addEventListener('submit', handleProfileFormSubmit); 
+
