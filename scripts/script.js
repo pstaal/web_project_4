37f7fa5b4de6979ed7c129d794e3caf50129557b
pstaal <peter.staal@gmail.com>
2022-02-21 10:49:33 +0100

    feature: add list of cards via data array

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,50 @@ let nameInput = document.querySelector("[name='name']");
 let expertiseInput = document.querySelector("[name='function']");
 let profileName = document.querySelector('.profile__name');
 let profileFunction = document.querySelector('.profile__function');
+let cardContainer = document.querySelector('.places');
 
+
+//initial cards 
+const initialCards = [
+    {
+      name: "Yosemite Valley",
+      link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+    },
+    {
+      name: "Lake Louise",
+      link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+    },
+    {
+      name: "Bald Mountains",
+      link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+    },
+    {
+      name: "Latemar",
+      link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+    },
+    {
+      name: "Vanoise National Park",
+      link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+    },
+    {
+      name: "Lago di Braies",
+      link: "https://code.s3.yandex.net/web-code/lago.jpg"
+    }
+  ]; 
+
+//create a card function 
+function createCard(data) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.places__card').cloneNode(true);
+    cardElement.querySelector('.places__card-image').setAttribute("src", data.link);
+    cardElement.querySelector('.places__card-title').textContent = data.name;
+    return cardElement;
+  } 
+
+initialCards.forEach((card) => {
+    let newCard = createCard(card);
+    cardContainer.append(newCard);
+});
 
 function openModal(){
     //set values of textinputs
