ba03695766c3dffdf348db5a4e59677e4d04dfab
pstaal <peter.staal@gmail.com>
2022-03-14 09:42:35 +0100

    feature: add validation logic forms

---
+++ scripts/validate.js
@@ -0,0 +1,79 @@
+// enabling validation by calling enableValidation()
+// pass all the settings on call
+
+  const showInputError = (formElement, inputElement, errorMessage,settings) => {
+    // Find the error message element inside the very function
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    // The rest remains unchanged
+    inputElement.classList.add(settings.inputErrorClass);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(settings.errorClass);
+  };
+  
+  const hideInputError = (formElement, inputElement,settings) => {
+    // Find the error message element
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    // The rest remains unchanged
+    inputElement.classList.remove(settings.inputErrorClass);
+    errorElement.classList.remove(settings.errorClass);
+    errorElement.textContent = "";
+  }; 
+
+
+
+
+  const isValid = (formElement, inputElement, settings) => {
+    if (!inputElement.validity.valid) {
+      // The parameter of showInputError() is now a form,
+      // which contains a field to be checked
+      showInputError(formElement, inputElement, inputElement.validationMessage, settings);
+    } else {
+      // The same for hideInputError(), Its parameter is now a form,
+      // which contains a field to be checked
+      hideInputError(formElement, inputElement, settings);
+    }
+  }; 
+
+  const setEventListeners = (formElement, settings) => {
+    // Find all fields inside the form, and
+    // make an array from them using the Array.from() method
+    const inputList = Array.from(formElement.querySelectorAll(settings.inputSelector));
+  
+    // Iterate over the resulting array
+    inputList.forEach((inputElement) => {
+      // add the input event handler to each field
+      inputElement.addEventListener("input", () => {
+        // Call the isValid() function inside the callback,
+        // and pass the form and the element to be checked to it
+        isValid(formElement, inputElement, settings)
+      });
+    });
+  }; 
+
+
+  const enableValidation = (settings) => {
+    // It will find all forms with the specified class in DOM, and
+    // make an array from them using the Array.from() method
+    const formList = Array.from(document.querySelectorAll(settings.formSelector));
+  
+    // Iterate over the resulting array
+    formList.forEach((formElement) => {
+      formElement.addEventListener("submit", (evt) => {
+        // Cancel default behavior for each form
+        evt.preventDefault();
+      });
+  
+      // Call the setEventListeners() function for each form,
+      // taking a form element as an argument
+      setEventListeners(formElement, settings);
+    });
+  };
+
+  enableValidation({
+    formSelector: ".popup__form",
+    inputSelector: ".popup__input",
+    submitButtonSelector: ".popup__button",
+    inactiveButtonClass: "popup__button_disabled",
+    inputErrorClass: "popup__input_type_error",
+    errorClass: "popup__error_visible"
+  }); 
