b7213be560ad30facbb4d488ddc856cb32f66e47
pstaal <peter.staal@gmail.com>
2022-04-07 17:21:40 +0200

    feature: add popup class

---
+++ scripts/Popup.js
@@ -0,0 +1,34 @@
+export default class Popup {
+    constructor(popupSelector){
+        this._popup = document.querySelector(popupSelector);
+    }
+
+    open() {
+        this._popup.classList.add('popup_opened');
+    }
+
+    close() {
+        this._popup.classList.remove('popup_opened');
+    }
+
+    _handleEscClose(evt) {
+        if (evt.key ==="Escape") {
+            this.close();
+          }
+    }
+    
+    setEventListeners() {
+        this._popup.querySelector('.popup__close').addEventListener('click', () => {
+            this.close();
+        });
+
+        document.addEventListener("keydown", this._handleEscClose);
+        document.addEventListener("click", (evt) => {
+            if (evt.target.classList.contains('popup') && document.querySelector(".popup_opened")) {
+                this.close();
+              }
+        });
+
+    }
+
+}
