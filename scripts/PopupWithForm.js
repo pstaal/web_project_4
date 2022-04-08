3cbb554adc0085c0d624f3b8cf827da80f878a4d
pstaal <peter.staal@gmail.com>
2022-04-08 11:54:09 +0200

    feature: add popupwithform class

---
+++ scripts/PopupWithForm.js
@@ -0,0 +1,77 @@
+import Popup from "./PopUp.js";
+
+class PopupWithForm extends Popup {
+    constructor(popupSelector, handleFormSubmit) {
+        super(popupSelector);
+        this._handleFormSubmit = handleFormSubmit;
+    }
+
+    _getInputValues() {
+        // Get all field elements
+        this._inputList = this._element.querySelectorAll(".form__input");
+
+        // Create an empty object
+        this._formValues = {};
+
+        // Add the values of the fields to this object
+        this._inputList.forEach(input => {
+            this._formValues[input.name] = input.value;
+        });
+
+        // Return the values object
+        return this._formValues;
+    } 
+
+    close() {
+        this._popup.querySelector('.popup__form').reset();
+        this._popup.classList.remove('popup_opened');
+    }
+
+
+    setEventListeners() {
+        this._popup.querySelector('.popup__close').addEventListener('click', () => {
+            this.close();
+        });
+
+        this._popup.querySelector('.popup__form').addEventListener('submit', (evt) => {
+            evt.preventDefault();
+            this._handleFormSubmit(this._getInputValues());
+            this.close();
+        });
+
+        document.addEventListener("keydown", this._handleEscClose);
+        document.addEventListener("click", (evt) => {
+            if (evt.target.classList.contains('popup') && document.querySelector(".popup_opened")) {
+                this.close();
+              }
+        });
+
+    }
+    
+
+}
+
+const formPlace = new PopupWithForm (
+    ".popup-place",
+     (data) => {
+        const placeTitle = data['title'];
+        const placeURL = data['link'];
+      
+        const newCard = renderCard({text: placeTitle, imageLink: placeURL}, "#card-template");
+        cardContainer.prepend(newCard);
+    }
+); 
+
+const formProfile = new PopupWithForm (
+    ".popup-profile",
+     (data) => {
+        const newName = data['name'];
+        const newExpertise = data['function'];
+      
+        profileName.textContent = newName;
+        profileFunction.textContent = newExpertise;
+    }
+); 
+
+
+export { formPlace , formProfile };
