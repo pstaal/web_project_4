377a75dd82e89a9e94065bb47ee7a2dfb0227c37
pstaal <peter.staal@gmail.com>
2022-04-08 09:55:19 +0200

    feature: add class popupwithimage

---
+++ scripts/PopupWithImage.js
@@ -0,0 +1,17 @@
+import Popup from "./PopUp";
+
+export default class PopupWithImage extends Popup {
+    constructor(popupSelector){
+        super(popupSelector);
+    }
+
+    open(imageLink, imageName){
+        const popupImage = document.querySelector('.popup-picture__image');
+        const popupImageTitle = document.querySelector('.popup-picture__title');
+        popupImage.setAttribute('src', imageLink);
+        popupImage.setAttribute('alt', imageName);    
+        popupImageTitle.textContent = imageName;
+        this._popup.classList.add('popup_opened');
+    } 
+
+}
