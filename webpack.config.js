825d895e2d8cb36ad240b11df57dc74338cbfec2
pstaal <peter.staal@gmail.com>
2022-04-11 15:20:28 +0200

    add html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,40 @@
 // webpack.config.js
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   devtool: 'inline-source-map',
   entry: {
-    main: './src/index.js'
+    main: "./src/index.js"
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-    publicPath: ''
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: ""
   },
-  target: ['web', 'es5'], // ensure the Webpack glue code is ES5 compatible too
-  mode: 'development',
+  target: ['web', 'es5'],
+  stats: { children: true },
+  mode: "development",
   devServer: {
-    static: path.resolve(__dirname, './dist'),
+    static: path.resolve(__dirname, "./dist"),
     compress: true,
     port: 8080,
     open: true
   },
   module: {
-    rules: [ // this is an array of rules
-      // add an object containing rules for Babel to it
+    rules: [
       {
-        // a regular expression that searches for all js files
         test: /\.js$/,
-        // all files must be processed by babel-loader
         loader: "babel-loader",
-        // exclude the node_modules folder, we don't need to process files in it
         exclude: "/node_modules/"
       }
     ]
-  }
-} 
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+    new CleanWebpackPlugin()
+  ],
+};
